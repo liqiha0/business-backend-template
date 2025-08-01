346c9f31a21eb7e71a38d031ab80f903af6fe48b
liqihao <liqiha0@outlook.com>
2025-08-01 10:23:56 +0800

    累积更新

--- core/src/main/kotlin/io/github/liqiha0/template/core/domain/config/ConfigurationKey.kt
+++ core/src/main/kotlin/io/github/liqiha0/template/core/domain/config/ConfigurationKey.kt
@@ -1,35 +1,17 @@
 package io.github.liqiha0.template.core.domain.config
 
-/**
- * Represents a strongly-typed key for a system configuration value.
- *
- * @param T The type of the value this key refers to.
- */
+import java.math.BigDecimal
+
 interface ConfigurationKey<T> {
-    /**
-     * The group of the key, typically the enum class name.
-     */
     val keyGroup: String
 
-    /**
-     * The name of the key, typically the enum constant name.
-     */
     val keyName: String
 
-    /**
-     * The expected data type of the configuration value.
-     */
     val valueType: Class<T>
 
-    /**
-     * The default value for this configuration.
-     */
     val defaultValue: T?
 }
 
-/**
- * A concrete implementation of [ConfigurationKey] for [String] values.
- */
 data class StringKey(
     override val keyGroup: String,
     override val keyName: String,
@@ -38,9 +20,6 @@ data class StringKey(
     override val valueType: Class<String> get() = String::class.java
 }
 
-/**
- * A concrete implementation of [ConfigurationKey] for [Int] values.
- */
 data class IntKey(
     override val keyGroup: String,
     override val keyName: String,
@@ -49,13 +28,18 @@ data class IntKey(
     override val valueType: Class<Int> get() = Int::class.java
 }
 
-/**
- * A concrete implementation of [ConfigurationKey] for [Boolean] values.
- */
 data class BooleanKey(
     override val keyGroup: String,
     override val keyName: String,
     override val defaultValue: Boolean? = null
 ) : ConfigurationKey<Boolean> {
     override val valueType: Class<Boolean> get() = Boolean::class.java
+}
+
+data class BigDecimalKey(
+    override val keyGroup: String,
+    override val keyName: String,
+    override val defaultValue: BigDecimal? = null
+) : ConfigurationKey<BigDecimal> {
+    override val valueType: Class<BigDecimal> get() = BigDecimal::class.java
 }
